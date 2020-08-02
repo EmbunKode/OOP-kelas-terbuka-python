525c538cc3d82ad309433443cbbff647d217d2b3
sigit <sigitghoticmetal2001@gmail.com>
2020-08-02 22:13:47 +0700

    latihan oop game saling serang

--- latihan_oop.py
+++ latihan_oop.py
@@ -7,6 +7,8 @@ Antara Hero Sniper melawan Hero Rikimaru
 
 class Hero:
 
+    # self adalah sebagai sebuah variable saja yang menyatakan kelas itu sendiri.
+    # untuk memanggil sebuah variable dan sebuah metode yang ada pada dirinya sendiri yang diawali dengan self.
     def __init__(self, name, health, attackPower, armorNumber):
         self.armorNumber = armorNumber
         self.name = name
@@ -20,22 +22,28 @@ class Hero:
 
     def diserang(self, lawan, attackPower_lawan):
         print(self.name + ' diserang ' + lawan.name)
+        # attack_diterima = 20 / 5
         attack_diterima = attackPower_lawan/self.armorNumber
-        print(f'serangan menghabiskan health sebesar = {attack_diterima}')
+        print(f'serangan terasa = ' + str(attack_diterima))
+        # darah atau health dikurang dengan attack_diterima
+        self.health -= attack_diterima
+        print('darah ' + self.name + ' tersisa ' + str(self.health))
 
     def bertahan(self, lawan):
         print(self.name + ' Bertahan ketika dilawan ' + lawan.name)
 
 
+# name: sniper, health: 100, attackPower: 10, armorNumber: 5
 sniper = Hero('sniper', 100, 10, 5)
-rikimaru = Hero('rikimaru', 100, 5, 10)
-ucup = Hero('ucup', 300, 40, 25)
+rikimaru = Hero('rikimaru', 100, 20, 10)
+# ucup = Hero('ucup', 300, 40, 25)
 
 sniper.serang(rikimaru)
+print('\n')
 rikimaru.serang(sniper)
-print('\n')
-ucup.serang(rikimaru)
-rikimaru.serang(ucup)
-print('\n')
-ucup.bertahan(rikimaru)
-sniper.bertahan(rikimaru)
+# print('\n')
+# ucup.serang(rikimaru)
+# rikimaru.serang(ucup)
+# print('\n')
+# ucup.bertahan(rikimaru)
+# sniper.bertahan(rikimaru)
